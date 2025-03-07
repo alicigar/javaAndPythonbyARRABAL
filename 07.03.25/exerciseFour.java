260c9beba890d781c2761d059532fdb9f0d150c9
alicigar <aliciamomoko@gmail.com>
2025-03-07 13:52:39 +0100

    ok

---
+++ 07.03.25/exerciseFour.java
@@ -0,0 +1,6 @@
+public class exerciseFour{
+    public static void main(String[] args){
+        
+    }
+    }
+}
---
+++ 07.03.25/exerciseOne.java
@@ -0,0 +1,11 @@
+public class exerciseOne {
+    public static void main (String[] args){
+        int num = 0;
+        if (num == 0)
+            System.out.println("zero");
+        else if (num > 0)
+            System.out.println("positive");
+        else
+            System.out.println("negative");
+    }
+}
